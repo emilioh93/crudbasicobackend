df415660fd97f062e2faafbbcb2b667251732fe8
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-15 17:48:33 -0300

    se agregó la configuración inicial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+// Setup
 // Creo una instancia de express
 const app = express();
 
@@ -8,3 +9,7 @@ const port = 4000;
 app.listen(port, () => {
   console.log("Estoy usando el puerto " + port);
 });
+
+// Herramientas extras
+
+// Rutas
