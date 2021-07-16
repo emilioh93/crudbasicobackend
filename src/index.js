64da368d9298da71c4553c69e2d98280e99269b7
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-16 19:20:02 -0300

    se agregaron middlewares e index.html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 
-// Setup
-// Creo una instancia de express
+/* -------------------------------------------------------------------------- */
+/*                                    Setup                                   */
+/* -------------------------------------------------------------------------- */
+/* ---------------------- Creo una instancia de express --------------------- */
 const app = express();
 
-const port = 4000;
+/* ------------------------------ Crear puerto ------------------------------ */
+app.set("port", process.env.PORT || 4000);
 
-app.listen(port, () => {
-  console.log("Estoy usando el puerto " + port);
+app.listen(app.get("port"), () => {
+  console.log("Estoy usando el puerto " + app.get("port"));
 });
 
-// Herramientas extras
+/* -------------------------------------------------------------------------- */
+/*                      Herramientas extras - Middlewares                     */
+/* -------------------------------------------------------------------------- */
+// Morgan nos da info detallada de las solicitudes
+app.use(morgan("dev"));
+// Cors nos permite hacer solicitudes remotas
+app.use(cors());
+app.use(express.json());
+// Para recibir objeto con datos
+app.use(express.urlencoded({ extend: true }));
+// Permite agregar rutas estáticas
+app.use(express.static(path.join(__dirname, "../public")));
 
-// Rutas
+/* -------------------------------------------------------------------------- */
+/*                                    Rutas                                   */
+/* -------------------------------------------------------------------------- */
+app.get("/", (req, res) => {
+  res.send("Hola desde el servidor");
+});
