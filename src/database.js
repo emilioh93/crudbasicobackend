df2bc4c94618029dfe889e8336568b1b6c6aed28
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-16 20:10:05 -0300

    se agregó la conexión a la base de datos

---
+++ src/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const url = "mongodb://localhost:27017/crudbasico";
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("BD conectada");
+});
